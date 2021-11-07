9d07c723e05bd5a0c8fb0aaa65b31c32976c8c6b
yanagi914 <yuto.4587@icloud.com>
2021-11-08 01:54:10 +0900

    テンプレートを作った

--- group9/urls.py
+++ group9/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+app_name = 'proffesional'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('proffesional/', include('proffesional.urls')),
