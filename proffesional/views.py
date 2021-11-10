7b1eb8baec80a9724181a33cec6cfe8b6d83c2e1
Naka0710 <19026164@mmm.muroran-it.ac.jp>
2021-11-10 19:16:12 +0900

    csvアップロードしてモデルに反映されるようにした(仮)

--- proffesional/views.py
+++ proffesional/views.py
@@ -28,6 +28,21 @@ def result(request):
     return render(request, template_file, option)
 
 def csv_test(request):
+    if 'csv' in request.FILES:
+        form_data = TextIOWrapper(request.FILES['csv'].file, encoding='ansi')
+        csv_file = csv.reader(form_data)
+        for line in csv_file:
+            my_grades, created =models.My_Grades.objects.get_or_create(subject_name=line[4])
+            my_grades.subject_name = line[4]
+            my_grades.subject_code = line[3]
+            my_grades.pass_or_fail = line[7]
+            my_grades.save()
+
+        return render(request, 'proffesional/csv_test.html')
+
+    else:
+        return render(request, 'proffesional/csv_test.html')
+
     template_file = "proffesional/csv_test.html"
 
     option = {
