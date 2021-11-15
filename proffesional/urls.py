c4d523671882b1d888ea7fae8aad8a3428e9e290
yanagi914 <yuto.4587@icloud.com>
2021-11-15 10:57:11 +0900

    main画面消した

--- proffesional/urls.py
+++ proffesional/urls.py
@@ -6,9 +6,6 @@ app_name = 'proffesional'
 urlpatterns = [
     #タイトル画面
     path('',views.title, name = 'title'),
-    #メイン画面
-    path('main',views.mainView, name = 'main'),
-
     #単位取得状況表示画面
     path('result',views.result, name = 'result'),
     #CSVアップロードテスト用
--- proffesional/views.py
+++ proffesional/views.py
@@ -11,13 +11,6 @@ def title(request):
     }
     return render(request, template_file, option)
 
-def mainView(request):
-    template_file = "proffesional/main.html"
-
-    option = {
-
-    }
-    return render(request, template_file, option)
 
 def result(request):
     template_file = "proffesional/result.html"
