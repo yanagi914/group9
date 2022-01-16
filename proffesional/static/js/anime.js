8268ff8cd336b5120deb472d6380f8a921774391
yanagi914 <yuto.4587@icloud.com>
2022-01-17 03:21:43 +0900

    9.5割完成！！

--- proffesional/static/js/anime.js
+++ proffesional/static/js/anime.js
@@ -73,3 +73,75 @@ $(window).scroll(function () {
 
     });
 });
+
+$(function(){
+    $('.text').children().addBack().contents().each(function() {
+      $(this).replaceWith($(this).text().replace(/(\S)/g, '<p><span>$&</span></p>'));
+    });
+  });
+  $(window).on('load', function(){
+    $(".text").addClass("active");
+  });
+
+  $(function(){
+    $(".check1").on('click',function(){
+      $(".is-open1").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check2").on('click',function(){
+      $(".is-open2").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check3").on('click',function(){
+      $(".is-open3").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check4").on('click',function(){
+      $(".is-open4").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check5").on('click',function(){
+      $(".is-open5").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check6").on('click',function(){
+      $(".is-open6").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check7").on('click',function(){
+      $(".is-open7").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check8").on('click',function(){
+      $(".is-open8").slideToggle();
+    });
+    
+  });
+
+  $(function(){
+    $(".check9").on('click',function(){
+      $(".is-open9").slideToggle();
+    });
+    
+  });
